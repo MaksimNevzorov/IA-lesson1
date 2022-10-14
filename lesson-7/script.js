6d66170c7194193dd1705258e9b0113164481469
Nevzorov Maksim <nevzorov.work@yandex.by>
2022-10-14 18:52:37 +0300

    lessons 9

---
+++ lesson-7/script.js
@@ -0,0 +1,16 @@
+const tabls = document.getElementById('age-list')
+const label = document.getElementsByTagName('label')
+const td = document.querySelector('td')
+const form = document.getElementsByName('search')[0]
+const form1 = document.getElementsByName('search')
+const firsteImput = form.firstElementChild.querySelector('input')
+const lastInput = form.lastElementChild
+ 
+
+
+console.log(tabls)
+console.log(label)
+console.log(td)
+console.log(form)
+console.log(firsteImput)
+console.log(lastInput)
